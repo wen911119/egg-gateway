ad2bc0cde99933b2782ed35d744e1f5103b3904b
wen911119 <wen911119@gmail.com>
2017-12-06 22:55:13 +0800

    hard code 写到配置文件里去

---
+++ config/config.local.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = appInfo => {
+    return {
+        consul: {
+            host: 'daily.quancheng-ec.com',
+            port: "8500",
+            groups: ['athena-service', 'ceres-service', 'zeus-service', 'minos-service', 'sparta-search', 'chaos-service', 'budget-service', 'samoyed-service']
+        },
+        grpc: {
+
+        }
+    }
+};
