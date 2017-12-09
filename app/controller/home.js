3ba5e138cf98e2b356c67e9512c9f050597f4707
wen911119 <wen911119@gmail.com>
2017-12-10 01:43:27 +0800

    提前保留app的引用，在实际调用时也同样能取到最新值。避免了调用时传入ctx

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,21 +3,21 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    console.log(Object.keys(this.app.grpc))
-    console.log(this.app.grpc.FormDataBinDingService.deleteOne)
-    try{
-        let ret = await this.app.grpc.FormDataBinDingService.deleteBinDingByFormDataIds(this.ctx, {
-            formDataIds:[]
-        })
-        console.log(ret)
-    }catch(e){
-        console.log(e, 444444444444444)
+    async index() {
+        console.log(Object.keys(this.app.grpc))
+        console.log(this.app.grpc.FormDataBinDingService.deleteOne)
+        try {
+            let ret = await this.app.grpc.FormDataBinDingService.deleteBinDingByFormDataIds({
+                formDataIds: []
+            })
+            console.log(ret)
+        } catch (e) {
+            console.log(e, 444444444444444)
+        }
+
+
+        this.ctx.body = 'hi, egg';
     }
-    
-    
-    this.ctx.body = 'hi, egg';
-  }
 }
 
 module.exports = HomeController;
